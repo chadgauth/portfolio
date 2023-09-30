e6a082cce20355dbd3c61d90940ea4309ce1b016
Chad Gauthier <chad.gauthier@crowdstreet.com>
2023-09-29 23:56:59 -0500

    add GA and update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown()]
 });
